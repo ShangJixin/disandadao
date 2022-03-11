751e9a100476b6173b75243e3f305bd8555cfd8c
尚寂新 <shangjixin@outlook.com>
2022-03-11 18:11:01 +0800

    remove fake link && add developer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -4,9 +4,10 @@
             <div class="content">
                 <div class="sub">
                     <strong>SDZJ V3.</strong>build.vue
+                    <br>Dev by <a href="https://github.com/ShangJixin/" target="_blank" title="三道水师提督尚寂新">ShangJixin</a>
                 </div>
                 Copyright &copy; 2013-{{ nowYear }}<br>
-                <strong><a href="https://www.disandadao.org/?lang=zh">第三大道公益服</a></strong> | 勿剽窃本服及本站创意
+                <strong><a href="https://www.disandadao.org/">第三大道公益服</a></strong> | 勿剽窃本服及本站创意
             </div>
         </div>
     </div>
