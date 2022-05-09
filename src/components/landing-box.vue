e77efb31bf59221286396e32c477b5a1a36a78a1
尚寂新 <shangjixin@outlook.com>
2022-05-09 23:39:24 +0800

    优化 首页landing手机端展现，使用flex重构

--- src/components/landing-box.vue
+++ src/components/landing-box.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="landing-box">
-        <div class="container">
+        <div class="container cover">
             <div class="landing">
                 <h1>时光荏苒，初心未改</h1>
                 <p>第三大道公益服 公益 认真 负责</p>
             </div>
+        </div>
+        <div class="container foobar">
             <div class="guide">向下翻动，查看更多&nbsp;&darr;</div>
         </div>
     </div>
@@ -17,6 +19,58 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.landing-box .container.cover {
+    height: 80vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+.landing-box .container.foobar {
+    height: 20vh;
+    line-height: 20vh;
+}
+.landing-box {
+    background: url(../assets/landing.jpg) center bottom no-repeat;
+    background-size: cover;
+}
+
+.landing-box .landing h1 {
+    font-size: 32px;
+    margin: 20px 0;
+}
+.landing-box .landing p {
+    font-size: 16px;
+    margin: 20px 0;
+}
+
+.landing-box .guide {
+    font-size: 16px;
+    color: #999;
+    user-select: none;
+}
+
+@media screen and (max-width: 500px) {
+    /* 手机端展示 限高，不能用100vh，不美观 */
+    /* 约定 60vh */
+    .landing-box .container.cover {
+        height: 60vh;
+    }
+    .landing-box .container.foobar {
+        /*
+        height: 20vh;
+        line-height: 20vh;
+        */
+        display: none;
+    }
+    .landing-box .container.cover {
+        justify-content: center;
+        text-align: center;
+    }
+    .landing-box {
+        background-size: contain;
+    }
+}
+/*
 .landing-box {
     user-select: none;
     height: 100vh;
@@ -52,4 +106,5 @@ export default {
     color: #999;
     text-transform: capitalize;
 }
+*/
 </style>
